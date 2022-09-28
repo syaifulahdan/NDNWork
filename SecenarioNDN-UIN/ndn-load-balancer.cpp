8df31c1cdffd9df74e60302af00f3ac2276b7bab
Syaiful Ahdan <syaifulahdan@gmail.com>
2022-09-28 21:16:32 +0700

    Update ndn-load-balancer.cpp

--- SecenarioNDN-UIN/ndn-load-balancer.cpp
+++ SecenarioNDN-UIN/ndn-load-balancer.cpp
@@ -1,24 +1,3 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-/**
- * Copyright (c) 2011-2015  Regents of the University of California.
- *
- * This file is part of ndnSIM. See AUTHORS for complete list of ndnSIM authors and
- * contributors.
- *
- * ndnSIM is free software: you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
- *
- * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
- * PURPOSE.  See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
- **/
-
-// ndn-load-balancer.cpp
-
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
 #include "ns3/ndnSIM-module.h"
@@ -27,30 +6,8 @@
 
 using namespace ns3;
 
-/**
- * This scenario simulates a load balancer topology (using topology reader module)
- *
- *               ( ) ----- ( ) ---- (consumer)
- *                |
- *        ------ ( ) -----
- *        |               |
- *    (producer) ---- (producer)
- *
- * All links are 1Mbps with propagation 10ms delay.
- *
- * FIB is populated using NdnGlobalRoutingHelper.
- *
- * Consumer requests data from the two producers with frequency 10 interests per
- * second (interests contain constantly increasing sequence number).
- *
- * For every received interest, a load balancing operation is performed
- * (based on a custom forwarding strategy) and the selected producer
- * replies with a data packet, containing 1024 bytes of virtual payload.
- *
- * To run scenario and see what is happening, use the following command:
- *
- *     NS_LOG=ndn.Consumer:ndn.Producer ./waf --run=ndn-load-balancer
- */
+
+
 
 using ns3::ndn::StackHelper;
 using ns3::ndn::AppHelper;
@@ -93,7 +50,7 @@ main(int argc, char* argv[])
 
   AppHelper consumerHelper("ns3::ndn::ConsumerCbr");
   consumerHelper.SetPrefix(prefix);
-  consumerHelper.SetAttribute("Frequency", StringValue("1000")); // 100 interests a second
+  consumerHelper.SetAttribute("Frequency", StringValue("1500")); // 100 interests a second
   consumerHelper.Install(consumerNodes);
 
   AppHelper producerHelper("ns3::ndn::Producer");
